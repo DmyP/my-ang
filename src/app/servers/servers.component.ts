68964f001dc9b384fb09c8e8fa93579118fa3974
Dmytro_Pechenyi <dmyp33@gmail.com>
2019-03-22 14:38:34 +0200

    initial commit2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName = 'TestString';
   serverCreated = false;
+  servers = ['server_1', 'server_1'];
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +27,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server ' + this.serverName + ' was created' ;
   }
 
